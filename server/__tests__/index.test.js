a219118ebf0ab62ea7743dc07eaf33ddbf44e0e2
Thiago <thiagolos1999@gmail.com>
2023-08-05 12:57:42 +0100

    feat: first socket test passing

--- server/__tests__/index.test.js
+++ server/__tests__/index.test.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import router from '../router';
+import router from '../router.js';
 import supertest from 'supertest';
-import dbModels from '../models/chatroom.model'
+import dbModels from '../models/chatroom.model.js'
 import mongoose from 'mongoose';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
@@ -11,19 +11,39 @@ const Chatroom = dbModels.Chatroom;
 const databaseName = 'test';
 
 describe ('Integration tests', () => {
+  let io, serverSocket, clientSocket;
   const app = express();
   app.use(express.json());
   app.use(router);
   const request = supertest(app);
 
   beforeAll(async () => {
+    const httpServer = createServer(app);
+    io = new Server(httpServer);
     const url = `mongodb://127.0.0.1/${databaseName}`;
     await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    httpServer.listen(() => {
+      const port = httpServer.address().port;
+      clientSocket = new Client(`http://localhost:${port}`);
+      io.on("connection", (socket) => {
+        serverSocket = socket;
+      });
+      clientSocket.on("connect", () => {
+        console.log('Client connected');
+      });
+    });
   });
 
-  afterEach(async () => {
+  beforeEach(async () => {
+    await Chatroom.deleteMany({});
+  });
+
+  afterAll(async () => {
     await Chatroom.collection.drop();
     mongoose.connection.close();
+    io.close();
+    clientSocket.close();
   });
 
   it('should create chatrooms, and retreive them from the database', async () => {
@@ -38,28 +58,6 @@ describe ('Integration tests', () => {
     response = await request.get('/chatrooms');
     expect(JSON.parse(response.text).length).toBe(2);
   })
-});
-
-describe ('Socket tests', () => {
-  let io, serverSocket, clientSocket;
-
-  beforeAll((done) => {
-    const httpServer = createServer();
-    io = new Server(httpServer);
-    httpServer.listen(() => {
-      const port = httpServer.address().port;
-      clientSocket = new Client(`http://localhost:${port}`);
-      io.on("connection", (socket) => {
-        serverSocket = socket;
-      });
-      clientSocket.on("connect", done);
-    });
-  });
-
-  afterAll(() => {
-    io.close();
-    clientSocket.close();
-  });
 
   test("should establish socket connection", (done) => {
     clientSocket.on("test", (arg) => {
@@ -67,5 +65,26 @@ describe ('Socket tests', () => {
       done();
     });
     serverSocket.emit("test", "works");
+  });
+
+  it("should join a room, or create one if it does not exist", (done) => {
+    const testRoom = {
+      name: "test room",
+      time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes(),
+      creator: "testerman"
+    }
+    serverSocket.on("create_room", async(roomName) => {
+
+      expect(roomName).toBe(testRoom.name);
+
+      dbModels.addOrUpdate(roomName);
+      let response = await request.get('/chatrooms');
+      expect(JSON.parse(response.text).length).toBe(1);
+
+
+      io.emit("update_chatrooms", await dbModels.getAll())
+      done();
+    })
+    clientSocket.emit("create_room", testRoom.name);
   });
 });
